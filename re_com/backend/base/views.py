d87391344303e8f7fa771d10f3f424fed0a9b46b
FayiZ33 <muhammedfayiz002@gmail.com>
2022-02-05 13:48:38 +0530

    first

---
+++ re_com/backend/base/views.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Product
+from .products import products
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+
+# Create your views here.
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
